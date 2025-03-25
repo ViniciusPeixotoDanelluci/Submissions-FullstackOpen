5d620cbf4f04245acf7a832921ce6a908ef287ba
Vinicius <62160146+ViniciusPeixotoDanelluci@users.noreply.github.com>
2025-03-25 12:21:10 -0300

    2.10
    small updates, and exercise done.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,34 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
-console.log(persons[0].number)
+const Filter = ({ filterPerson, setFilterPerson}) => { 
+  const handlePersonSearch = (event) => {
+    //setFilterPerson(event.target.value) // It has the asynchronous problem from the part1
+    
+    const attFilterPerson = event.target.value
+    setFilterPerson(attFilterPerson)
+  }
+
+  return (
+    <div>
+      filter: <input
+        value={filterPerson}
+        onChange={handlePersonSearch}
+      />
+    </div>
+  )
+}
+
+const PersonForm = ({persons, setPersons}) => {
   const [newPerson, setNewPerson] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [showPerson, setShowPerson] = useState(true)
-  const [filterPerson, setFilterPerson] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
+    if (newPerson === '' || newNumber === '') {
+      alert('Name or number is missing')
+      return
+    }
+  
     const nameObject = {
       name: newPerson,
       number: newNumber,
@@ -34,59 +48,67 @@ console.log(persons[0].number)
   const handleNameUpdate = (event) => {
     setNewPerson(event.target.value)
   }
+
   const handleNumberUpdate = (event) => {
     setNewNumber(event.target.value)
   }
+
+  return ( 
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input 
+          value={newPerson}
+          onChange={handleNameUpdate}
+        />
+      </div>
+      <div>
+        number: <input 
+          value={newNumber}
+          onChange={handleNumberUpdate}
+        />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = ({ personsToShow }) => { 
+  return (
+    <ul>
+      {personsToShow.map(person => 
+        <li key={person.id}>{person.name} {person.number}</li>
+      )}
+    </ul>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
+
+  const [filterPerson, setFilterPerson] = useState('')
+
   const personsToShow = filterPerson
     ? persons.filter(p => p.name.toLowerCase().includes(filterPerson.toLowerCase()))
     : persons
-  
-  const handlePersonSearch = (event) => {
-    //filterPerson = event.target.value // It has the asynchronous problem from the part1
-    
-    const attFilterPerson = event.target.value
-
-    setFilterPerson(attFilterPerson)
-    console.log("filter Person:", filterPerson)
-
-    console.log("att filter Person:", attFilterPerson)
-    console.log(personsToShow)
-  }
 
   return (
     <div>
-      <div>Debug: {newPerson} {newNumber}</div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          filter: <input
-            value={filterPerson}
-            onChange={handlePersonSearch}
-          />
-        </div>
-        <h2>Add New</h2>
-        <div>
-          name: <input 
-            value={newPerson}
-            onChange={handleNameUpdate}
-          />
-        </div>
-        <div>
-          number: <input 
-            value={newNumber}
-            onChange={handleNumberUpdate}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter filterPerson={filterPerson} setFilterPerson={setFilterPerson}/>
+
+      <h2>Add New</h2>
+      <PersonForm persons={persons} setPersons={setPersons}/>
+      
+      <p>Formulario componentizado:</p>
       <h2>Numbers</h2>
-      <ul>
-        {personsToShow.map(person => 
-          <li key={person.id}>{person.name} {person.number}</li>
-        )}
-      </ul>
+      <Persons personsToShow={personsToShow} />
     </div>
   )
 }
