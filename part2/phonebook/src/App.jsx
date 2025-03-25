5dc8f9dfade255e3df7ebf6090da7f3c8ab5a36f
Vinicius <62160146+ViniciusPeixotoDanelluci@users.noreply.github.com>
2025-03-25 01:09:08 -0300

    small updates

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '55-18-998992074', id: 1 },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
 console.log(persons[0].number)
   const [newPerson, setNewPerson] = useState('')
