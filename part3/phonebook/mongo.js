6daaa711c4f901cf1da7f363e476d2d2e2844f66
Vinicius <62160146+ViniciusPeixotoDanelluci@users.noreply.github.com>
2025-09-15 19:59:12 -0300

    Exercise 3.12
    Done.
    
    Revert "Exercise 3.12"
    
    This reverts commit 9a8cbc9904ccb9216945e2b2a382e571efadd175.
    
    Reapply "Exercise 3.12"
    
    This reverts commit c6cdba284eca552067cb80fa484d84d49ded94da.

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -1,30 +1,53 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('give password as argument')
+  console.log('Give atleast a password as argument')
   process.exit(1)
 }
 
 const password = process.argv[2]
+const iName = process.argv[3]
+const iNumber = process.argv[4]
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.glbeuwq.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url = `mongodb+srv://fullstack:${password}@cluster0.glbeuwq.mongodb.net/personApp?retryWrites=true&w=majority`
 mongoose.set('strictQuery',false)
 
 mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Person = mongoose.model('Person', personSchema)
 
-const note = new Note({
-  content: 'HTML is easy',
-  important: true,
-})
+if (process.argv.length == 5) {
+  const person = new Person({
+    name: iName,
+    number: iNumber,
+  })
 
-note.save().then(result => {
-  console.log('note saved!')
+  person
+    .save()
+    .then(result => {
+      console.log(`${iName} added to the phonebook!`)
+      mongoose.connection.close()
+    }).catch((err) => {
+      console.log(err) 
+      mongoose.connection.close()
+    })
+} else if (process.argv.length == 3) {
+  Person.find({})
+    .then(result => {
+      console.log('phonebook:') 
+      result.forEach(person => {
+        console.log(person.name, person.number)
+      })
+      mongoose.connection.close()
+    })
+} else {
+  console.log('Please provide a name and number as arguments')
   mongoose.connection.close()
-})
+  process.exit(1)
+}
+
